f7d00ff7bfa67f4572621ef4ddf2a91070fcbeec
rwGast <robert.w.gast@gmail.com>
2025-09-01 14:06:59 -0700

    change constants.py

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    gameClock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -16,5 +18,8 @@ def main():
             
         screen.fill((0,0,0))
         pygame.display.flip()
+
+        dt = gameClock.tick(60)/1000
+
 if __name__ == "__main__":
     main()
